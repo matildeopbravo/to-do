db1deb949eae6c260b6e22a70d6fd0ee7989af82
matildeopbravo <56790511+matildeopbravo@users.noreply.github.com>
2019-12-02 19:26:17 +0000

    Add files via upload

---
+++ db/migrate/20191126210144_add_is_completed_to_todo.rb
@@ -0,0 +1,5 @@
+class AddIsCompletedToTodo < ActiveRecord::Migration[6.0]
+  def change
+    # add_column :todos, :isCompleted, :boolean
+  end
+end
---
+++ db/migrate/20191126230020_add_idto_todo.rb
@@ -0,0 +1,4 @@
+class AddIdtoTodo < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
